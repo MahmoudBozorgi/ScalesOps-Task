283d2416b837c0a9bef66c430708979bb8c87129
mahmoudbozorgi <mahmoud.bzri@gmail.com>
2023-12-28 02:14:21 +0330

    install react error boundary and use lazy loading.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import NavigationBar from "./components/NavigationBar";
-import Main from "./pages/Main";
-import Shop from "./pages/Shop";
-import Journal from "./pages/Journal";
-import About from "./pages/About";
 import { Provider } from "react-redux";
 import Store from "./redux/Store";
+import Loading from "./components/Loading";
+
+const Main = lazy(() => import("./pages/Main"));
+const Shop = lazy(() => import("./pages/Shop"));
+const Journal = lazy(() => import("./pages/Journal"));
+const About = lazy(() => import("./pages/About"));
 
 function App() {
   return (
     <>
       <Provider store={Store}>
-        <Routes>
-          <Route path="/" element={<NavigationBar />}>
-            <Route index element={<Main />} />
-            <Route path="/shop" element={<Shop />} />
-            <Route path="/journal" element={<Journal />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/main" />
-          </Route>
-        </Routes>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={<NavigationBar />}>
+              <Route index element={<Main />} />
+              <Route path="/shop" element={<Shop />} />
+              <Route path="/journal" element={<Journal />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/main" />
+            </Route>
+          </Routes>
+        </Suspense>
       </Provider>
     </>
   );
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,19 @@
-import React from "react";
-import Products from "../components/Products";
+import React, { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "../components/ErrorBoundary";
+import Loading from "../components/Loading";
+
+const Products = React.lazy(() => import("../components/Products"));
 
 function Main() {
   return (
-    <>
-      <Products />
-    </>
+    <div>
+      <ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => {}}>
+        <Suspense fallback={<Loading />}>
+          <Products />
+        </Suspense>
+      </ErrorBoundary>
+    </div>
   );
 }
 
